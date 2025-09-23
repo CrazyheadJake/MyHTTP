67d272dcdc426ac1c889891b230c6b7373a78d83
CrazyheadJake <jake.d.moleski@gmail.com>
2025-09-22 22:22:02 -0700

    Setting up environment

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+}
